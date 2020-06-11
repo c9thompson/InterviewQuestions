e0509ac2e244ba8ef15fd218367051538b84433e
Christopher Thompson <c9thomps@gmail.com>
2020-06-11 13:32:25 -0400

    simplified sort color

--- JuneCodingChallenge/SortColors.cpp
+++ JuneCodingChallenge/SortColors.cpp
@@ -24,23 +24,20 @@ public:
         
         int index = 0;
         while(index <= end){
-            while(nums[index] != 1 && index >= start && index <= end){
-                if(nums[index] == 0){
-                    nums[index] = nums[start];
-                    nums[start] = 0;
+            if(nums[index] == 0){
+                nums[index] = nums[start];
+                nums[start] = 0;
 
-                    start++;
-                }
+                start++;
+                index++;
+            }else if(nums[index] == 2){
+                nums[index] = nums[end];
+                nums[end] = 2;
 
-                if(nums[index] == 2){
-                    nums[index] = nums[end];
-                    nums[end] = 2;
-
-                    end--;
-                }
+                end--;
+            }else{            
+                index++;
             }
-            
-            index++;
         }
     }
 };
