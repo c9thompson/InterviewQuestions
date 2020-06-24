e03b8191aa491078eb12d1e1515bde9f89b67f31
Christopher Thompson <c9thomps@gmail.com>
2020-06-24 16:55:55 -0400

    unique binary trees

---
+++ JuneCodingChallenge/UniqueBinarySearchTrees.cpp
@@ -0,0 +1,31 @@
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+
+Example:
+
+Input: 3
+Output: 5
+Explanation:
+Given n = 3, there are a total of 5 unique BST's:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+class Solution {
+public:
+    int numTrees(int n) {
+        if(!n) return 1;
+        
+        vector<int> output(n + 1, 0 );
+        output[0] = output[1] = 1;
+        for(int i = 2; i <= n; ++i)
+            for(int j = i - 1; j >= 0; --j)
+                output[i] += output[j]*output[i - j - 1];
+        
+        return output.back();
+    }
+};
